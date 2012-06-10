cd1141fb14f6fb4c6e9b6e7f029fd5b26ad8ba8f
Richard Dean <rdean@dius.com.au>
2012-06-10 16:34:11 +1000

    Screwing around with the runner output formatting

--- runner.rb
+++ runner.rb
@@ -9,27 +9,30 @@ vga = {:sku => 'vga', :name => 'Vga adapter', :price => 3000  }
 # create our rules
 rules = [
   BulkPurchase.new('ipd', 4, 49999),
-  BuyXGetAnotherProductFree.new('mbp', 1, 'vga'),
+  BuyXGetAnotherProductFree.new('mbp', 1, 'vga', vga[:name]),
   BuyXGetXFree.new('atv', 2, 1)
 ]
 
 ci = Checkout.new rules
 [atv, atv, atv, vga].each {|item| ci.scan item}
-ci.items.each{|item| puts "#{item.name} - #{item.price}"}
-puts '-'*20
-puts "Cart Total: #{ci.total}"
-puts '-'*20
+ci.items.each{|item| puts "#{item.name} - $#{item.price}"}
+puts '-'*40
+puts "Cart Total: $#{ci.total}"
+puts '-'*40
+puts
 
 ci = Checkout.new rules
 [atv, ipd, ipd, atv, ipd, ipd, ipd].each {|item| ci.scan item}
-ci.items.each{|item| puts "#{item.name} - #{item.price}"}
-puts '-'*20
-puts "Cart Total: #{ci.total}"
-puts '-'*20
+ci.items.each{|item| puts "#{item.name} - $#{item.price}"}
+puts '-'*40
+puts "Cart Total: $#{ci.total}"
+puts '-'*40
+puts
 
 ci = Checkout.new rules
 [mbp, vga, ipd].each {|item| ci.scan item}
-ci.items.each{|item| puts "#{item.name} - #{item.price}"}
-puts '-'*20
-puts "Cart Total: #{ci.total}"
-puts '-'*20
+ci.items.each{|item| puts "#{item.name} - $#{item.price}"}
+puts '-'*40
+puts "Cart Total: $#{ci.total}"
+puts '-'*40
+puts
